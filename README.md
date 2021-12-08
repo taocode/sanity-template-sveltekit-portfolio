73cf50cf63380240a0099cb4dee0e4c672587a2d
Mark Jones <mark@taocode.com>
2021-12-07 22:39:02 -0500

    fix: project page begins to have members and category listings

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# sanity-template-gatsby-portfolio
+# sanity-template-sveltekit-portfolio
 
-_Clean React.js starting point for portfolios, project listings or case studies._
+_Clean SvelteKit starting point for portfolios, project listings or case studies._
 
-[See demo](https://sanity-template-gatsby-portfolio-web.netlify.com/).
+[See demo](https://sanity-template-sveltekit-portfolio-web.netlify.com/).
 
-This template repo is used by Sanity.io to easily create deployed and configured projects through a web interface. You can test it out by [creating this project](https://www.sanity.io/create/?template=sanity-io%2Fsanity-template-gatsby-portfolio).
+This template repo is used by Sanity.io to easily create deployed and configured projects through a web interface. You can test it out by [creating this project](https://www.sanity.io/create/?template=taocode%2Fsanity-template-sveltekit-portfolio).
 
-The template contains both the Sanity Studio and a front-end in Gatsby. Both are deployed on Netlify.
+The template contains both the Sanity Studio and a front-end in SvelteKit. Both are deployed on Netlify.
 
 Want to make a template for Sanity and your favourite front-end framework? We’re still maturing [sanity.io/create](https://sanity.io/create) and our APIs, but do tell us about it on [slack.sanity.io](https://slack.sanity.io).
 
-![The Gatsby and Sanity powered portfolio frontend](https://github.com/sanity-io/sanity-template-gatsby-portfolio/blob/main/assets/frontend.jpg?raw=true)
+![The SvelteKit and Sanity powered portfolio frontend](https://github.com/taocode/sanity-template-sveltekit-portfolio/blob/main/assets/frontend.jpg?raw=true)
 
 ## Local development
 
@@ -20,9 +20,9 @@ You develop the templates in `/template`, and review your changes in `/build`.
 
 2. **Run `npm run dev` in root folder.** This will build the template files to `/build`. This is how the code will look for those who install the project later.
 
-3. **Run `npm install` in `./build/web` and `sanity install` in `/build/studio`** This will install the necessary dependencies for the Gatsby front-end and the Studio.
+3. **Run `npm install` in `./build/web` and `sanity install` in `/build/studio`** This will install the necessary dependencies for the SvelteKit front-end and the Studio.
 
-4. **Run `npm run dev` in `./build/web` and `sanity start` in `/build/studio`**. This will start the development servers for the Gatsby front-end and Sanity Studio.
+4. **Run `npm run dev` in `./build/web` and `sanity start` in `/build/studio`**. This will start the development servers for the SvelteKit front-end and Sanity Studio.
 
 ## Notes
 
--- template/web/src/routes/project/[slug].svelte
+++ template/web/src/routes/project/[slug].svelte
@@ -7,10 +7,10 @@ export const load: Load = async ({fetch, page}) => {
       // console.log(page.params.slug,{page})
       const res = await fetch(`/project/${page.params.slug}.json`)
       // console.log({res})
-      const post = await res.json()
-      // console.log({post})
+      const project = await res.json()
+      // console.log({project})
       return {
-				props: { post }
+				props: { project }
 			}
     } catch (err) {
       console.log('blog load error',err)
@@ -28,25 +28,82 @@ export const load: Load = async ({fetch, page}) => {
   import urlBuilder from '@sanity/image-url';
   import client from '$lib/sanityClient';
   import Image from "$lib/Image.svelte";
+  import { format, parseISO, differenceInDays, formatDistance } from 'date-fns'
 
-  export let post;
+  export let project;
   const urlFor = source => urlBuilder(client).image(source);
 
-  $: console.log(post)
-$: heroImgURL = urlFor(post.mainImage).width(1200).height(675).fit('crop')
-
+  const heroImgURL = urlFor(project.mainImage).width(1200).height(675).fit('crop')
+  const publishedAt = project.publishedAt
+  const publishDate = differenceInDays(parseISO(publishedAt), new Date()) > 3
+                  ? formatDistance(parseISO(publishedAt), new Date())
+                  : format(parseISO(publishedAt), "MMMM do yyyy")
+  console.log(project.categories)
+  const categories = project.categories.map(category => 
+            `<li key=${category._id}>${category.title}</li>`
+          )
 </script>
 
 <svelte:head>
-  <title>{post.title}</title>
+  <title>{project.title}</title>
 </svelte:head>
-<Image url={heroImgURL} alt={post.mainImage.alt} />
-<h1>{post.title}</h1>
-<div class="content">
-  <BlockContent blocks={post.body} {serializers} />
+<Image url={heroImgURL} alt={project.mainImage.alt} />
+
+<div class="container">
+  <div class="grid">
+    <div class="content-main">
+      <h1>{project.title}</h1>
+      <div class="content">
+        <BlockContent blocks={project.body} {serializers} />
+      </div>
+    </div>
+    <div class="content-meta">
+      <div class="meta-date">{publishDate}</div>
+      <div class="meta-item">
+        <h2>Project members</h2>
+        {#each project.members as m}
+        <BlockContent blocks={m} {serializers} />
+        {/each}
+        
+      </div>
+      <div class="meta-item">
+        <h3>Categories</h3>
+        {#each project.members as m}
+        <BlockContent blocks={m} {serializers} />
+        {/each}
+        <ul>
+          {#each categories as c}
+          <div class="">{@html c}</div>
+          {/each}
+        </ul>
+      </div>
+    </div>
+  </div>
 </div>
   
 <style>
+  .meta-date {
+    color: var(--color-gray);
+    margin: 1.5em 0 3em;
+  }
+  .meta-item {
+    border-top: 2px solid var(--color-gray-very-light);
+    margin: 2em 0 3em;
+  }
+  .container {
+    max-width: 960px;
+    margin: 0 auto;
+  }
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-column-gap: 2em;
+  }
+  @media (min-width: 675px) {
+    .grid {
+      grid-template-columns: 3fr 1fr;
+    }
+  }
   .content :global(h2) {
     font-size: 1.4em;
     font-weight: 500;
